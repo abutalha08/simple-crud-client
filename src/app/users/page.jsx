6c40242ecf18fe5bb4418bef30f9219c6908ff78
abutalha08 <taufique752@gmail.com>
2026-05-15 02:27:34 +0600

    Display user data in client-side UI

---
+++ src/app/users/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { getUsers } from '../lib/data';
+import UsersTable from '../components/UsersTable';
+
+const page = async () => {
+    const users = await getUsers();
+    return (
+        <div>
+            <div className='flex justify-between'>
+                <h2>User Management: {users.length}</h2>
+                {/* <AddUserModal createUserAction = {createUser}></AddUserModal> */}
+            </div>
+            <UsersTable users={users} > </UsersTable>
+        </div>
+    );
+};
+
+export default page;
