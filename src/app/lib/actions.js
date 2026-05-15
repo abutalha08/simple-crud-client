965105a5f111c5f34e8c1417409daf14fb98952a
abutalha08 <taufique752@gmail.com>
2026-05-15 12:47:11 +0600

    Display add user modal and Create operation and set POST operation

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,5 +1,34 @@
 import { revalidatePath } from "next/cache";
 
+
+
+
+
+export const createUser = async (formData) => {
+    'use server';
+    const newUser = Object.fromEntries(formData.entries());
+
+    // console.log('new user data', newUser)
+
+    const res = await fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    })
+    const data = await res.json();
+    console.log('data after post', data);
+
+    if (data.insertedId) {
+        revalidatePath('/users');
+    }
+
+    return data;
+}
+
+
+
 export const deleteUser = async (userId) => {
     'use server';
 
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { getUsers } from '../lib/data';
 import UsersTable from '../components/UsersTable';
-import { deleteUser } from '../lib/actions';
+import { createUser, deleteUser } from '../lib/actions';
+import AddUserModal from '../components/AddUserModal';
 
 const page = async () => {
     const users = await getUsers();
@@ -9,7 +10,7 @@ const page = async () => {
         <div>
             <div className='flex justify-between'>
                 <h2>User Management: {users.length}</h2>
-                {/* <AddUserModal createUserAction = {createUser}></AddUserModal> */}
+                <AddUserModal createUserAction = {createUser}></AddUserModal>
             </div>
             <UsersTable users={users} deleteUserAction={deleteUser} > </UsersTable>
         </div>
